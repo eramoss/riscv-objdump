45162884822dbec1389cc6b3b8b61f0adf21bb7d
eramoss <edulramos@outlook.com>
2025-05-07 08:46:45 -0300

    better solution for nops

--- src/nops.c
+++ src/nops.c
@@ -7,22 +7,14 @@ void handle_nops(rscv_asm_words asmw, int opt){
 	hazard_t* hazard_list = (hazard_t*)malloc(sizeof(hazard_t) * len);
 	int num_hazards = identify_hazards(insts, len, hazard_list);
 	
-	int last_hazard = -1;
 	for(int i = 0; i < len; i++){
 		printf("0x%08x\n", asmw.words[i]);
-		for(int j= 0; j< num_hazards; j++) {
-			if(last_hazard != i && hazard_list[j].hazard_with == i){
-				last_hazard = i;
-				if (hazard_list[j].type == CTRL){
-					for(int num_nops = 0; num_nops <3; num_nops++)
-						printf("0x00000013  ; auto generated\n");
-					break;
-				}
-				else if (hazard_list[j].type == DATA){
-					for(int num_nops = 0; num_nops <2; num_nops++)
-						printf("0x00000013  ; auto generated\n");
-					break;
-				}
+		for(int j= num_hazards-1; j > -1; j--) {
+			hazard_t h = hazard_list[j];
+			if(h.hazard_with == i){
+				for(int num_nops = 0; num_nops < (h.index - h.hazard_with); num_nops++)
+					printf("0x00000013  ; nop\n");
+				break;
 			}
 		}
 	}
